b36691afb9e8841cd3f96a7eb74599a3b21ec102
kunal171 <kunalkunal63@gmail.com>
2023-11-27 18:09:56 +0530

    getting response back

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,17 +1,25 @@
 use mini_redis::client;
 use tokio::sync::mpsc;
 use bytes::Bytes;
+use tokio::sync::oneshot;
+
 
 #[derive(Debug)]
 enum Command {
     Get {
         key: String,
+        resp: Responder<Option<Bytes>>,
     },
     Set {
         key: String,
         val: Bytes,
+        resp: Responder<()>,
     }
 }
+
+/// Provided by the requester and used by the manager task to send
+/// the command response back to the requester.
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
 async fn main() {
@@ -21,20 +29,33 @@ async fn main() {
 
     // Spawn two tasks, one gets a key, the other sets a key
     let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Get {
             key: "foo".to_string(),
+            resp: resp_tx,
         };
-
+        // Send the GET request
         tx.send(cmd).await.unwrap();
+
+        // Await the response
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
     });
 
     let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
         let cmd = Command::Set {
             key: "foo".to_string(),
             val: "bar".into(),
+            resp: resp_tx,
         };
 
+        //Send the SET request
         tx2.send(cmd).await.unwrap();
+
+        // Await the response
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
     });
 
     // while let Some(message) = rx.recv().await {
@@ -48,14 +69,17 @@ async fn main() {
 
         // Start receiving messages
         while let Some(cmd) = rx.recv().await {
-            use Command::*;
-
+            
             match cmd {
-                Get { key } => {
-                    client.get(&key).await;
+                Command::Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    // Ignore errors
+                    let _ = resp.send(res);
                 }
-                Set { key, val } => {
-                    client.set(&key, val).await;
+                Command::Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+                    // Ignore errors
+                    let _ = resp.send(res);
                 }
             }
         }
