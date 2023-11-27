297e2a240693b7a927b10e8812e05783835dbf3b
kunal171 <kunalkunal63@gmail.com>
2023-11-27 17:40:12 +0530

    spawn Manager task

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,19 +1,69 @@
 use mini_redis::client;
+use tokio::sync::mpsc;
+use bytes::Bytes;
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    // Establish a connection to the server
-    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
 
     // Spawn two tasks, one gets a key, the other sets a key
-    let t1 = tokio::spawn(async {
-        let res = client.get("foo").await;
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+        };
+
+        tx.send(cmd).await.unwrap();
     });
 
-    let t2 = tokio::spawn(async {
-        client.set("foo", "bar".into()).await;
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+        };
+
+        tx2.send(cmd).await.unwrap();
+    });
+
+    // while let Some(message) = rx.recv().await {
+    //     println!("GOT = {}", message);
+    // }
+
+    // The `move` keyword is used to **move** ownership of `rx` into the task.
+    let manager = tokio::spawn(async move {
+        // Establish a connection to the server
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+        // Start receiving messages
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+
+            match cmd {
+                Get { key } => {
+                    client.get(&key).await;
+                }
+                Set { key, val } => {
+                    client.set(&key, val).await;
+                }
+            }
+        }
     });
 
     t1.await.unwrap();
     t2.await.unwrap();
+    manager.await.unwrap();
+    
 }
+
